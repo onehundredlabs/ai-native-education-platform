5d7932400893780f0c956ee161e3c852cc096813
Patrik Braborec <patrikbraborec@gmail.com>
2025-03-10 18:31:40 +0100

    Add react-query and basic layout
    Signed-off-by: Patrik Braborec <patrikbraborec@gmail.com>

---
+++ client-web/src/components/custom/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="h-10 border-b border-gray-200 flex items-center justify-between">
+      <div className="container mx-auto">
+        <h1>AI-native education platform</h1>
+      </div>
+    </header>
+  );
+};
--- client-web/src/routes/__root.tsx
+++ client-web/src/routes/__root.tsx
@@ -1,15 +1,23 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
-import Header from '../components/Header'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Header } from "../components/custom/header";
+import { LeftPanel } from "@/components/custom/left-panel";
+import { RightPanel } from "@/components/custom/right-panel";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Header />
 
-      <Outlet />
+      <div className="container mx-auto flex justify-between">
+        <LeftPanel />
+
+        <Outlet />
+
+        <RightPanel />
+      </div>
+
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
